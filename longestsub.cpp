104c83d5d991dbc5c0901e0c54e7aacc447ec60f
DornishApril <samireformed786@gmail.com>
2024-11-20 19:10:12 +0600

    first file

---
+++ longestsub.cpp
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+
+#include<iostream>
+#include<string>
+#include<string.h>
+
+using namespace std;
+
+int main(){
+    cout<<"IDGAF";
+}
