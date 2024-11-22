b517d15388ae5af374d59a7921024484741f2901
DornishApril <samireformed786@gmail.com>
2024-11-23 00:12:39 +0600

    tried something with while loop, utterly disgusting. rolling back

--- checknum.cpp
+++ checknum.cpp
@@ -1,32 +1,43 @@
 #include "headers.h"
 
 int main(){
-    unordered_map<int,int> charmap;
-
-    string a = " ";
+    
+    string a = "dvdf";
 
     int current_length=0;
     int highest_length=0;
+    int quit=0;
+    int j=0;
 
-    for(int i=0;a[i]!='\0';i++){
-        cout<<(int)a[i]<<"\n";
-        cout<<"----"<<charmap[(int)a[i]]<<"---\n";
-        charmap[(int)a[i]]+=1;
+    while(!quit){
 
-        if(charmap[(int)a[i]]==2){
+        unordered_map<char,int> charmap;
+        int i=j;
+
+    for(int i=j;a[i]!='\0';i++){
+        //cout<<(int)a[i]<<"\n";
+        if(a[i]=='\0')
+            quit =1;
+        cout<<i<<"\n";
+        charmap[a[i]]+=1;
+
+        if(charmap[a[i]]==2){
             highest_length = (highest_length<current_length)? (current_length):(highest_length);
             current_length = 0;
             charmap.clear();
-            i=i-1;
+            j=i-2;
+            break;
+            
 
         }
         else{
-            
             current_length++;
         }
     }
+}
+    highest_length = (highest_length<current_length)? (current_length):(highest_length);
 
-    cout<<highest_length;
+    cout<<"---"<<highest_length;
     
 
 }
